876550f13f2242824169ef4095e5dc0b60396b08
Jordi Pages <jordi.pages@pal-robotics.com>
2016-10-04 17:02:16 +0200

    initial commit

---
+++ scripts/navigation.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+#
+# Check if the $HOME/.pal folder is available and creates it if needed before
+# launching navigation.
+#
+# Usage: $0 <robot> [<state>] [<localization method>] [<mapping method>] [<map>]
+
+# Check parameters:
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <robot> [<state>] [<localization method>] [<mapping method>] [<map>] [<octomap>]"
+  echo "Check if the $HOME/.pal folder is availabe and creates it if needed"
+  echo "before launching navigation."
+  exit 1
+else
+  ROBOT=$1
+fi
+
+if [ $# -lt 2 ]; then
+  STATE=localization
+else
+  STATE=$2
+fi
+
+if [ $# -lt 3 ]; then
+  LOCALIZATION=amcl
+else
+  LOCALIZATION=$3
+fi
+
+if [ $# -lt 4 ]; then
+  MAPPING=gmapping
+else
+  MAPPING=$4
+fi
+
+if [ $# -lt 5 ]; then
+  MAP=$HOME/.pal/${ROBOT}_maps/config
+else
+  MAP=$5
+fi
+
+#if [ -d "$MAP" -a "$STATE" = "mapping" ]; then
+#  rm -rf $MAP
+#fi
+
+if [ ! -d "$MAP" ]; then
+  mkdir -p $MAP
+  if [ $? -ne 0 ]; then
+   echo "Error: Failed to create path $MAP"
+    exit 3
+  fi
+fi
+
+# Run localization/mapping
+roslaunch ${ROBOT}_2dnav $STATE.launch localization:=$LOCALIZATION mapping:=$MAPPING map:=$MAP
+
+
+
